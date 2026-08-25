6eaa9103c160a69942c7d4022c642041ffd95fab
reubenp <reuben.pinkhasov@gmail.com>
2026-08-25 09:20:24 +0300

    Add GitHub Actions AWS OIDC configuration

---
+++ terraform/github-ecr-policy.tf
@@ -0,0 +1,40 @@
+resource "aws_iam_policy" "github_actions_ecr" {
+  name        = "devops-portfolio-github-actions-ecr"
+  description = "Allow GitHub Actions to push images to the portfolio ECR repository"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "ECRLogin"
+        Effect = "Allow"
+
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+
+        Resource = "*"
+      },
+      {
+        Sid    = "ECRPush"
+        Effect = "Allow"
+
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+
+        Resource = aws_ecr_repository.app.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_ecr" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions_ecr.arn
+}
